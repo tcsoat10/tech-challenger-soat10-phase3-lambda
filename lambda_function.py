956d4f473a99f56d4bbf34cf31cea1832210dc5d
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-31 20:06:36 -0300

    feat: add user creation on cognito

--- lambda_function.py
+++ lambda_function.py
@@ -15,44 +15,80 @@ def validate_cpf(cpf_value: str) -> bool:
 
 def lambda_handler(event, context):
     data = json.loads(event['body'])
-    cpf = data.get('cpf')
 
-    if not cpf:
-        return {
-            'statusCode': HTTPStatus.BAD_REQUEST.value,
-            'body': 'Missing CPF'
-        }
-    
-    if not validate_cpf(cpf):
-        return {
-            'statusCode': HTTPStatus.BAD_REQUEST.value,
-            'body': 'Invalid CPF'
-        }
-
-    user_pool_id = os.getenv('USER_POOL_ID')
-    if not user_pool_id:
-        return {
-            'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            'body': 'Server configuration error: USER_POOL_ID not set.'
-        }
+    if 'name' in data and 'email' in data and 'birthdate' in data and 'cpf' in data:
+        user_pool_id = os.getenv('USER_POOL_ID')
+        if not user_pool_id:
+            return {
+                'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR.value,
+                'body': 'Server configuration error: USER_POOL_ID not set.'
+            }
+            
+        cognito_service = CognitoService(user_pool_id=user_pool_id)
         
-    cognito_service = CognitoService(user_pool_id=user_pool_id)
+        cpf = data.get('cpf')
+        try:
+            exists_user = cognito_service.exists_user_in_user_pool(cpf)
+            if exists_user is True:
+                return {
+                    'statusCode': HTTPStatus.CONFLICT.value,
+                    'body': 'User already exists, log in with CPF'
+                }
+            else:
+                new_user = cognito_service.create_user_in_user_pool(data)
+                if new_user is True:
+                    return {
+                        'statusCode': HTTPStatus.CREATED.value,
+                        'body': 'User created successfully'
+                    }
+        except Exception as e:
+            return {
+                'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR.value,
+                'body': f'Error checking user: {str(e)}'
+            }
     
-    try:
-        exists_user = cognito_service.exists_user_in_user_pool(cpf)
-        if exists_user is True:
-            return {
-                'statusCode': HTTPStatus.OK.value,
-                'body': 'User exists'
-            }
-        else:
-            return {
-                'statusCode': HTTPStatus.NOT_FOUND.value,
-                'body': 'User does not exist'
-            }
-    except Exception as e:
-        return {
-            'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            'body': f'Error checking user: {str(e)}'
-        }
+    elif 'cpf' in data and not 'name' in data and not 'email' in data and not 'birthdate' in data:
+        cpf = data.get('cpf')
 
+        if not cpf:
+            return {
+                'statusCode': HTTPStatus.BAD_REQUEST.value,
+                'body': 'Missing CPF'
+            }
+        
+        if not validate_cpf(cpf):
+            return {
+                'statusCode': HTTPStatus.BAD_REQUEST.value,
+                'body': 'Invalid CPF'
+            }
+
+        user_pool_id = os.getenv('USER_POOL_ID')
+        if not user_pool_id:
+            return {
+                'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR.value,
+                'body': 'Server configuration error: USER_POOL_ID not set.'
+            }
+            
+        cognito_service = CognitoService(user_pool_id=user_pool_id)
+        
+        try:
+            exists_user = cognito_service.exists_user_in_user_pool(cpf)
+            if exists_user is True:
+                return {
+                    'statusCode': HTTPStatus.OK.value,
+                    'body': 'User exists'
+                }
+            else:
+                return {
+                    'statusCode': HTTPStatus.NOT_FOUND.value,
+                    'body': 'User does not exist'
+                }
+        except Exception as e:
+            return {
+                'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR.value,
+                'body': f'Error checking user: {str(e)}'
+            }
+    return {
+        'statusCode': HTTPStatus.BAD_REQUEST.value,
+        'body': 'Invalid request format'
+    }
